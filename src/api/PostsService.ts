b84b040ef2ef0a7c5edd51e369e156a896ebb225
Yankovskiy Georgiy <admin@yageorgiy.ru>
2023-12-17 02:22:48 +0300

    Env variables

--- src/api/PostsService.ts
+++ src/api/PostsService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class PostsService {
     static async getAll(limit: number = 10, page: number = 1) {
-        return await axios.get('https://jsonplaceholder.typicode.com/posts', {
+        return await axios.get(`${process.env.REACT_APP_MOCK_API_BASE_URL}/posts`, {
             params: {
                 _limit: limit,
                 _page: page
@@ -11,10 +11,10 @@ export default class PostsService {
     }
 
     static async getById(id: number) {
-        return await axios.get('https://jsonplaceholder.typicode.com/posts/' + id);
+        return await axios.get('${process.env.REACT_APP_MOCK_API_BASE_URL}/posts/' + id);
     }
 
     static async getCommentsByPostId(id: number) {
-        return await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+        return await axios.get(`${process.env.REACT_APP_MOCK_API_BASE_URL}/posts/${id}/comments`);
     }
 }
